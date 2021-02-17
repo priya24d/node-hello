19423c113f96cc80246022534a22b17d969bf7ca
priya24d <76624748+priya24d@users.noreply.github.com>
2021-02-17 15:52:43 +0530

    changes in index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hi Testing!\n'
+  const msg = 'Hello node!\n'
   res.end(msg);
 });
 
